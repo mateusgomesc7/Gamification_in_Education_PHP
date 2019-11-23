2489ec5aa38461f45aeefc0ec42770b78e9d9a05
Mateus Gomes <mateusgomesc7@gmail.com>
2019-11-23 16:27:58 -0300

    Administrator com login para acessar

---
+++ adm/app/adms/Controllers/Home.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\adms\Controllers;
+
+if (!defined('URL')) {
+    header("Location: /");
+    exit();
+}
+
+class Home
+{
+    private $Dados;
+
+    public function index()
+    {
+        $listarMenu = new \App\adms\Models\AdmsMenu();
+        $this->Dados['menu']= $listarMenu->itemMenu();
+        
+        $carregarView = new \Core\ConfigView("adms/Views/home/home", $this->Dados);
+        $carregarView->renderizar();
+    }
+
+}
---
+++ adm/core/ConfigView.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core;
+
+class ConfigView
+{
+
+    private $Nome;
+    private $Dados;
+
+    public function __construct($Nome, array $Dados = null)
+    {
+        $this->Nome = (string) $Nome;
+        $this->Dados = $Dados;
+    }
+
+    public function renderizar()
+    {
+        include 'app/adms/Views/include/cabecalho_adm.php';
+        include 'app/adms/Views/include/header.php';
+        include 'app/adms/Views/include/sidebar.php';
+        if (file_exists('app/' . $this->Nome . '.php')) {
+            include 'app/' . $this->Nome . '.php';
+        }else{
+            echo "Erro ao carregar a Página: {$this->Nome}";
+        }
+        include 'app/adms/Views/include/rodape_adm.php';
+    }
+
+    public function renderizarLogin()
+    {
+        include 'app/adms/Views/include/cabecalho.php';
+        if (file_exists('app/' . $this->Nome . '.php')) {
+            include 'app/' . $this->Nome . '.php';
+        }else{
+            echo "Erro ao carregar a Página: {$this->Nome}";
+        }
+    }
+
+}
